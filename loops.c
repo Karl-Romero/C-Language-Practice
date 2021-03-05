0b303e98fdcab7fb67eee264acb3149ce6c61fd5
Karl-Romero <senseikarl69@gmail.com>
2021-03-05 13:16:45 +0800

    my learnings

---
+++ loops.c
@@ -0,0 +1,14 @@
+#include <cs50.c>
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 2;
+    
+    while (i < 69)
+    {
+        printf("hello world\n");
+        // we can make this i++ if we want to add 1 to a variable
+        i++;
+    }
+}
